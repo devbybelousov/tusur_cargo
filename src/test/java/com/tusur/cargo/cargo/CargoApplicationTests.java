67036e6cfec032906832110957e0949cdb0bf838
Dmitry Belousov <devbybelousov@yandex.ru>
2021-03-17 13:35:45 +0700

    Создал базовый проект

---
+++ src/test/java/com/tusur/cargo/cargo/CargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusur.cargo.cargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
